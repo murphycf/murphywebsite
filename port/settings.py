48fe5a410933fa4e4f2c93166a32664b26fe60e7
Cole Murphy <93554309+murphycf@users.noreply.github.com>
2022-03-27 12:28:38 -0500

    Update settings.py

--- port/settings.py
+++ port/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+
 import dj_database_url
 from decouple import config
 
@@ -133,4 +133,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+
